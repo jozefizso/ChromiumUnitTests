ca45f08aabb4f257ca9b16eaf41d74ca699d504b
Jozef Izso <jozef.izso@gmail.com>
2020-08-08 19:17:53 +0200

    Sample test for 64-bit Windows

--- ChromiumTests64/SampleTest.cs
+++ ChromiumTests64/SampleTest.cs
@@ -1,12 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using CefSharp;
+using CefSharp.OffScreen;
+using NUnit.Framework;
 
 namespace ChromiumTests64
 {
+    [TestFixture]
     public class SampleTest
     {
+        [Test]
+        public void Method1()
+        {
+            var timeout = TimeSpan.FromSeconds(5);
+            var tempDir = Path.GetTempPath();
+            var baseDir = TestContext.CurrentContext.TestDirectory;
+
+            var settings = new CefSettings()
+            {
+                BrowserSubprocessPath = Path.Combine(baseDir, "CefSharp.BrowserSubprocess.exe"),
+                WindowlessRenderingEnabled = true,
+                LogFile = "chromium.log",
+                LogSeverity = LogSeverity.Verbose
+            };
+            CefSharpSettings.ShutdownOnExit = true;
+
+            Cef.Initialize(settings, performDependencyCheck: false, browserProcessHandler: null);
+            var browser = new ChromiumWebBrowser("https://tls-v1-2.badssl.com:1012/");
+            SpinWait.SpinUntil(() => browser.IsBrowserInitialized, timeout);
+
+            Assert.IsNotNull(browser);
+            Assert.IsTrue(browser.IsBrowserInitialized);
+        }
     }
 }
--- README.md
+++ README.md
@@ -2,6 +2,34 @@
 
 > Sample projects for running CefSharp Chromium inside unit test projects.
 
+## Getting Started
+
+1. Download the `nunit3-console` from [NUnit Console and Engine 3.11.1](https://github.com/nunit/nunit-console/releases/tag/v3.11.1)
+2. Extract it to `C:\tools`
+3. Set the `%PATH%` variable to include the `c:\tools\NUnit.Console-3.11.1\bin\net35` directory
+
+### ChromiumTests64 Sample
+
+> Requires Windows 64-bit operating system.
+
+The **ChromiumTests64** project shows how to run CefSharp in unit tests library
+on Windows 64-bit.
+
+The solution and project must target the **x64** platform. This is a limitation
+of CefSharp that whole solution must use **x64** platform.
+
+The `ChromiumTests64.dll` can be executed in `nunit3-console.exe`.
+As the console runs in 64-bit mode, this will ensure the whole process can
+load CefSharp correctly.
+
+Use **nunit3-console.exe** to run tests without isolation:
+
+```powershell
+$env:Path="c:\tools\NUnit.Console-3.11.1\bin\net35;"+$env:path
+cd ChromiumTests64\bin\Debug
+nunit3-console.exe --inprocess --domain=None .\ChromiumTests64.dll
+```
+
 ## License
 
 Licensed under [MIT License](LICENSE.txt).
